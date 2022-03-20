122740b04d36e6d284e02b48a342253a0a1bed9f
Assem <Assem@DESKTOP-AOSJQJN>
2022-03-20 11:03:00 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Азамат";
+            string surname = "Тулендинов";
+
+            Console.WriteLine(name +" "+ surname);
+            (name, surname) = (surname, name);
+            Console.WriteLine(name + " " + surname);
+        }
+    }
+}
